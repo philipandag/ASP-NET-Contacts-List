3578d4bceb2ee701d755a8f11ddb69cd8616a889
Filip Gołaś <philipgoclever@gmail.com>
2024-06-08 11:57:20 +0200

    Add project files.

---
+++ ASP-NET-Contacts-List/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_NET_Contacts_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
